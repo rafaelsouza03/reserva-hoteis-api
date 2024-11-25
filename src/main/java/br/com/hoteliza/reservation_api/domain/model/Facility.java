0cf9f278672d7763f59d27886e2414d870fdf5f1
Rafael Souza <rafael.souza29184@gmail.com>
2024-11-25 10:14:41 -0300

    implementando classe modelo Hotel

---
+++ src/main/java/br/com/hoteliza/reservation_api/domain/model/Facility.java
@@ -0,0 +1,26 @@
+package br.com.hoteliza.reservation_api.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Facility {
+	
+	GYM("Academia"),
+	POOL("Piscina"),
+	PRIVATE_PARKING("Estacionamento privativo"),
+	TOILETRIES("Artigos de higiene Pessoal"),
+	WIFI("Wi-Fi grátis"),
+	BREAKFEST("Café da manhã"),
+	LIBRARY("Biblioteca"),
+	BAR("Bar"),
+	SPA("Spa"),
+	MULTI_COURT("Quadra poliesportiva"),
+	PLAYGROUND("Parque infantil"),
+	ATM("Caixa 24h"),
+	FUMOIR("Fumódromo");
+
+	private String description;
+	
+}
---
+++ src/main/java/br/com/hoteliza/reservation_api/domain/model/Hotel.java
@@ -0,0 +1,62 @@
+package br.com.hoteliza.reservation_api.domain.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "hotels")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Hotel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nome", nullable = false)
+	private String name;
+	
+	@ElementCollection
+	@CollectionTable(name = "comodidades", joinColumns = @JoinColumn(name = "hotel_id"))
+	private List<Facility> facilites;
+	
+	@Column(name = "endereço", nullable = false)
+	private String address;
+	
+	//private List<String> pictures;
+	
+	@ElementCollection
+	@CollectionTable(name = "proximidades", joinColumns = @JoinColumn(name = "hotel_id"))
+	private List<String> vicinities;
+	
+	@Column(name = "politicas")
+	private String policy;
+	
+	@ManyToOne
+	@JoinColumn(name = "proprietario_id")
+	private HotelOwner owner;
+	
+	@OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL)
+    private List<Room> rooms;
+	
+}
