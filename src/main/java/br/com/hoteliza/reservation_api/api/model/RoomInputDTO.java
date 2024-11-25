f5645b8a5d9678358d7deee58c633d75787acb43
Rafael Souza <rafael.souza29184@gmail.com>
2024-11-25 12:20:59 -0300

    Criando DTOs de Room

---
+++ src/main/java/br/com/hoteliza/reservation_api/api/model/RoomInputDTO.java
@@ -0,0 +1,25 @@
+package br.com.hoteliza.reservation_api.api.model;
+
+import br.com.hoteliza.reservation_api.domain.model.Hotel;
+import br.com.hoteliza.reservation_api.domain.model.RoomType;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RoomInputDTO {
+
+	@NotBlank(message = "Escolha o tipo de quarto.")
+	private RoomType type;
+	
+	@NotBlank(message = "Digite a quantidade de camas.")
+	private Integer bedAmount;
+	
+	@NotBlank(message = "Digite o preço da diária.")
+	private Double pricePerDay;
+	
+	@NotBlank(message = "Escolha em qual hotel está o quarto.")
+	private Hotel hotel;
+	
+}
---
+++ src/main/java/br/com/hoteliza/reservation_api/api/model/RoomOutputDTO.java
@@ -0,0 +1,22 @@
+package br.com.hoteliza.reservation_api.api.model;
+
+import br.com.hoteliza.reservation_api.domain.model.Hotel;
+import br.com.hoteliza.reservation_api.domain.model.RoomType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RoomOutputDTO extends RoomBaseDTO{
+	
+	private RoomType roomType;
+	
+	private Integer bedAmount;
+	
+	private Integer peopleCapacity;
+	
+	private Double pricePerDay;
+	
+	private Hotel hotel;
+
+}
