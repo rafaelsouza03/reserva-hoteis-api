be5bbe721fd8c5fff788cdf28ee67f98178e994e
Rafael Souza <rafael.souza29184@gmail.com>
2024-11-25 10:55:46 -0300

    Implementando repositório e serviço de Reservation

---
+++ src/main/java/br/com/hoteliza/reservation_api/domain/service/ReservationService.java
@@ -0,0 +1,47 @@
+package br.com.hoteliza.reservation_api.domain.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.hoteliza.reservation_api.domain.model.Reservation;
+import br.com.hoteliza.reservation_api.domain.repository.ReservationRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class ReservationService {
+
+	private ReservationRepository reservationRepository;
+	
+	@Transactional
+	public Reservation register(final Reservation reservation) {
+		return reservationRepository.save(reservation);
+	}
+	
+	@Transactional
+	public Reservation update(final Reservation reservation, Long reservationId) {
+		Reservation findReservation = search(reservationId);
+		
+		findReservation.setCustomer(reservation.getCustomer());
+		findReservation.setRoom(reservation.getRoom());
+		findReservation.setStatus(reservation.getStatus());
+		findReservation.setTotalPrice(reservation.getTotalPrice());
+		findReservation.setCheckIn(reservation.getCheckIn());
+		findReservation.setCheckOut(reservation.getCheckOut());
+		
+		return reservationRepository.save(findReservation);
+	}
+	
+	@Transactional
+	public Reservation search(Long reservationId) {
+		return reservationRepository.findById(reservationId).orElseThrow(() -> new EntityNotFoundException("Reserva não encontrada")); 
+	}
+	
+	@Transactional
+	public List<Reservation> list(){
+		return reservationRepository.findAll();
+	}
+}
