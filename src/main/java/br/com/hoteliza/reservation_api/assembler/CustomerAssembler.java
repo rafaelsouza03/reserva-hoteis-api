9d45c1600ea3030a49e1ab111c96e4ad1695563f
Rafael Souza <rafael.souza29184@gmail.com>
2024-11-25 11:29:01 -0300

    Criando CustomerAssembler

---
+++ src/main/java/br/com/hoteliza/reservation_api/assembler/CustomerAssembler.java
@@ -0,0 +1,32 @@
+package br.com.hoteliza.reservation_api.assembler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import br.com.hoteliza.reservation_api.api.model.CustomerInputDTO;
+import br.com.hoteliza.reservation_api.api.model.CustomerOutputDTO;
+import br.com.hoteliza.reservation_api.domain.model.Customer;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Component
+public class CustomerAssembler {
+
+	private ModelMapper modelMapper;
+	
+	public Customer toCustomer(final CustomerInputDTO customerInputDTO) {
+		return modelMapper.map(customerInputDTO, Customer.class);
+	}
+	
+	public CustomerOutputDTO toDto(final Customer customer) {
+		return modelMapper.map(customer, CustomerOutputDTO.class);
+	}
+	
+	public List<CustomerOutputDTO> toCollectionDto(final List<Customer> customers){
+		return customers.stream().map(this::toDto).collect(Collectors.toList());
+	}
+	
+}
