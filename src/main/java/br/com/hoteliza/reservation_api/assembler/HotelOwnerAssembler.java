53676a6a18685659a8f1cf95e9f0eb76985f067c
Rafael Souza <rafael.souza29184@gmail.com>
2024-11-25 11:44:41 -0300

    Criando Assembler do HotelOwner

---
+++ src/main/java/br/com/hoteliza/reservation_api/assembler/HotelOwnerAssembler.java
@@ -0,0 +1,32 @@
+package br.com.hoteliza.reservation_api.assembler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import br.com.hoteliza.reservation_api.api.model.HotelOwnerInputDTO;
+import br.com.hoteliza.reservation_api.api.model.HotelOwnerOutputDTO;
+import br.com.hoteliza.reservation_api.domain.model.HotelOwner;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Component
+public class HotelOwnerAssembler {
+
+	private ModelMapper modelMapper;
+
+	public HotelOwner toHotelOwner(final HotelOwnerInputDTO hotelOwnerInputDTO) {
+		return modelMapper.map(hotelOwnerInputDTO, HotelOwner.class);
+	}
+
+	public HotelOwnerOutputDTO toDto(final HotelOwner hotelOwner) {
+		return modelMapper.map(hotelOwner, HotelOwnerOutputDTO.class);
+	}
+
+	public List<HotelOwnerOutputDTO> toCollectionDto(final List<HotelOwner> hotelOwners) {
+		return hotelOwners.stream().map(this::toDto).collect(Collectors.toList());
+	}
+	
+}
