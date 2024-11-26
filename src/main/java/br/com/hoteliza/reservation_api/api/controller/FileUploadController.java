8d16befd2e6bdd90f0d1403909cb853296624c96
Rafael Souza <rafael.souza29184@gmail.com>
2024-11-26 20:47:57 -0300

    Criando controller do FileUpload

---
+++ src/main/java/br/com/hoteliza/reservation_api/api/controller/FileUploadController.java
@@ -0,0 +1,10 @@
+package br.com.hoteliza.reservation_api.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/file")
+public class FileUploadController {
+
+}
