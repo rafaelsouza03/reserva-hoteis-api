4a3f2e06ffd6abaffcf42a5b227235722ef9eeff
Rafael Souza <rafael.souza29184@gmail.com>
2024-11-27 19:48:31 -0300

    Criando StorageFileNotFoundException

---
+++ src/main/java/br/com/hoteliza/reservation_api/storage/StorageFileNotFoundException.java
@@ -0,0 +1,18 @@
+package br.com.hoteliza.reservation_api.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
