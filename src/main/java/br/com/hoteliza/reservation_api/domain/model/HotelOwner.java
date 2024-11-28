74636cef157e6ac6809cefb7a8c0feb25855af13
Rafael Souza <rafael.souza29184@gmail.com>
2024-11-28 14:54:28 -0300

    Adicionando metodos requeridos pelo UserDetails

--- src/main/java/br/com/hoteliza/reservation_api/domain/model/HotelOwner.java
+++ src/main/java/br/com/hoteliza/reservation_api/domain/model/HotelOwner.java
@@ -1,6 +1,11 @@
 package br.com.hoteliza.reservation_api.domain.model;
 
+import java.util.Collection;
 import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -10,10 +15,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Table(name = "hotel_owners")
@@ -21,14 +26,49 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
-@Builder
-public class HotelOwner extends User {
-	
+@SuperBuilder
+public class HotelOwner extends User implements UserDetails {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
 	private List<Hotel> hotel;
-	
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority("ROLE_OWNER"));
+	}
+
+	@Override
+	public String getUsername() {
+		return getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
 }
