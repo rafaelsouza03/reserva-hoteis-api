adfe72e4e9b3f8e106f03c4265fe599dd9bb2186
Rafael Souza <rafael.souza29184@gmail.com>
2024-11-28 14:50:27 -0300

    Criando repositório da superclasse User

--- src/main/java/br/com/hoteliza/reservation_api/domain/repository/HotelOwnerRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.hoteliza.reservation_api.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.hoteliza.reservation_api.domain.model.HotelOwner;
-
-public interface HotelOwnerRepository extends JpaRepository<HotelOwner, Long>{
-
-}
