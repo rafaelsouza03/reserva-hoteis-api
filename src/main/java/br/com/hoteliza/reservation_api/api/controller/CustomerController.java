a76812407e6e91cd6e3e091dec392ed6ffa91b8b
Rafael Souza <rafael.souza29184@gmail.com>
2024-11-28 14:56:35 -0300

    Removendo metodos inuteis

--- src/main/java/br/com/hoteliza/reservation_api/api/controller/CustomerController.java
+++ src/main/java/br/com/hoteliza/reservation_api/api/controller/CustomerController.java
@@ -1,20 +1,15 @@
 package br.com.hoteliza.reservation_api.api.controller;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.hoteliza.reservation_api.api.model.CustomerInputDTO;
-import br.com.hoteliza.reservation_api.api.model.CustomerOutputDTO;
+import br.com.hoteliza.reservation_api.api.model.user.customer.CustomerInputDTO;
+import br.com.hoteliza.reservation_api.api.model.user.customer.CustomerOutputDTO;
 import br.com.hoteliza.reservation_api.assembler.CustomerAssembler;
 import br.com.hoteliza.reservation_api.domain.service.CustomerService;
 import jakarta.validation.Valid;
@@ -23,17 +18,11 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/customers")
+@RequestMapping("/customer")
 public class CustomerController {
 
 	private CustomerAssembler customerAssembler;
 	private CustomerService customerService;
-
-	@PostMapping("/create")
-	@ResponseStatus(HttpStatus.CREATED)
-	public CustomerOutputDTO getMethodName(@RequestBody final @Valid CustomerInputDTO customerInputDTO) {
-		return customerAssembler.toDto(customerService.register(customerAssembler.toCustomer(customerInputDTO)));
-	}
 
 	@PutMapping("/{customerId}")
 	public ResponseEntity<CustomerOutputDTO> update(@PathVariable final @NotNull Long customerId,
@@ -45,11 +34,6 @@ public class CustomerController {
 	@GetMapping("/{customerId}")
 	public CustomerOutputDTO consult(@PathVariable final @NotNull Long customerId) {
 		return customerAssembler.toDto(customerService.search(customerId));
-	}
-
-	@GetMapping("/list")
-	public List<CustomerOutputDTO> list() {
-		return customerAssembler.toCollectionDto(customerService.list());
 	}
 
 }
--- src/main/java/br/com/hoteliza/reservation_api/api/controller/HotelOwnerController.java
+++ src/main/java/br/com/hoteliza/reservation_api/api/controller/HotelOwnerController.java
@@ -1,20 +1,15 @@
 package br.com.hoteliza.reservation_api.api.controller;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.hoteliza.reservation_api.api.model.HotelOwnerInputDTO;
-import br.com.hoteliza.reservation_api.api.model.HotelOwnerOutputDTO;
+import br.com.hoteliza.reservation_api.api.model.user.hotel_owner.HotelOwnerInputDTO;
+import br.com.hoteliza.reservation_api.api.model.user.hotel_owner.HotelOwnerOutputDTO;
 import br.com.hoteliza.reservation_api.assembler.HotelOwnerAssembler;
 import br.com.hoteliza.reservation_api.domain.service.HotelOwnerService;
 import jakarta.validation.Valid;
@@ -23,18 +18,11 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/hotel_owners")
+@RequestMapping("/hotel_owner")
 public class HotelOwnerController {
 
 	private HotelOwnerAssembler hotelOwnerAssembler;
 	private HotelOwnerService hotelOwnerService;
-
-	@PostMapping("/create")
-	@ResponseStatus(HttpStatus.CREATED)
-	public HotelOwnerOutputDTO getMethodName(@RequestBody final @Valid HotelOwnerInputDTO hotelOwnerInputDTO) {
-		return hotelOwnerAssembler
-				.toDto(hotelOwnerService.register(hotelOwnerAssembler.toHotelOwner(hotelOwnerInputDTO)));
-	}
 
 	@PutMapping("/{hotelOwnerId}")
 	public ResponseEntity<HotelOwnerOutputDTO> update(@PathVariable final @NotNull Long hotelOwnerId,
@@ -46,11 +34,6 @@ public class HotelOwnerController {
 	@GetMapping("/{hotelOwnerId}")
 	public HotelOwnerOutputDTO consult(@PathVariable final @NotNull Long hotelOwnerId) {
 		return hotelOwnerAssembler.toDto(hotelOwnerService.search(hotelOwnerId));
-	}
-
-	@GetMapping("/list")
-	public List<HotelOwnerOutputDTO> list() {
-		return hotelOwnerAssembler.toCollectionDto(hotelOwnerService.list());
 	}
 
 }
